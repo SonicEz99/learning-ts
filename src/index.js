5244503cfeb2ab87e51dc07b0d0b2a863bde28d6
ZonixEz <pnem2003@gmail.com>
2025-03-11 01:30:56 +0700

    basic ts

---
+++ src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const num = 20;
+console.log(num);
+const str = "yaya";
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const num: number = 20;
+
+console.log(num);
+
+let str = "yaya"; //let มันรู้ว่าเป๋น string เพราะว่า อนุมาณได้จาก ตัว ""
+const strr = "yaya"
+
+
+// string
+// number
+// bigint
+// boolean
+// null
+// undefined
+// symbol
